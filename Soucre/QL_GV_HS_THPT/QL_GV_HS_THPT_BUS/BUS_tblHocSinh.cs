a5ae31dca21989e06fbf0de900dd02b62e0e4977
manhlinhitk49 <phongmanhlinh9502@gmail.com>
2016-10-06 21:14:17 +0700

    linh2

---
+++ Soucre/QL_GV_HS_THPT/QL_GV_HS_THPT_BUS/BUS_tblHocSinh.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QL_GV_HS_THPT_DAL;
+using QL_QV_HS_THPT_Entity;
+using System.Data;
+
+namespace QL_GV_HS_THPT_BUS
+{
+    public class BUS_tblHocSinh
+    {
+        SQL_tblHocsinh bus = new SQL_tblHocsinh();
+        public void addHocsinh(EC_tblHocsinh et)
+        {
+            bus.addHocsinh(et);
+        }
+        //Sua du lieu
+        public void updateHocSinh(EC_tblHocsinh et)
+        {
+            bus.updateHocsinh(et);
+        }
+        //Xoa du lieu
+        public void delHocSinh(EC_tblHocsinh et)
+        {
+            bus.delHocsinh(et);
+        }
+        //select
+        public DataTable getAllHocSInh()
+        {
+            return bus.getAllHocsinh();
+        }
+        public DataTable getHocSinh(string dk)
+        {
+            return bus.getHocsinh(dk);
+        }
+        public DataTable DoDLMaLop(string dk)
+        {
+            return bus.DoDLMaLop(dk);
+        }
+        
+    }
+}
--- Soucre/QL_GV_HS_THPT/QL_GV_HS_THPT_DAL/SQL_tblHocsinh.cs
+++ Soucre/QL_GV_HS_THPT/QL_GV_HS_THPT_DAL/SQL_tblHocsinh.cs
@@ -14,12 +14,14 @@ namespace QL_GV_HS_THPT_DAL
         //Them du lieu
         public void addHocsinh(EC_tblHocsinh et)
         {
-            cn.ThucThiCauLenhSQL(@"INSERT INTO tblHocsinh	");
+            cn.ThucThiCauLenhSQL(@"INSERT INTO tblHocSinh (MaHS, Ho, Ten, GT, NgaySinh, DiaChi, MaLop) 
+                                VALUES (N'"+et.MaHS+"',N'"+et.Ho+"',N'"+et.Ten+"',N'"+et.GT+"',N'"+et.NgaySinh+"',N'"+et.DiaChi+"',N'"+et.MaLop+"')	");
         }
         //Sua du lieu
         public void updateHocsinh(EC_tblHocsinh et)
         {
-            cn.ThucThiCauLenhSQL(@"UPDATE	tblHocsinh	SET   ");
+            cn.ThucThiCauLenhSQL(@"UPDATE tblHocSinh SET Ho =N'" + et.Ho + "', Ten =N'" + et.Ten + "', GT =N'" +
+                et.GT + "', NgaySinh =N'" + et.NgaySinh + "', DiaChi =N'" + et.DiaChi + "', MaLop =N'" + et.MaLop + "' where MaHS=N'"+et.MaHS+"'");
         }
         //Xoa du lieu
         public void delHocsinh(EC_tblHocsinh et)
@@ -34,6 +36,10 @@ namespace QL_GV_HS_THPT_DAL
         public DataTable getHocsinh(string dk)
         {
             return cn.getDatatable(@"SELECT * FROM tblHocsinh " + dk);
+        }
+        public DataTable DoDLMaLop(string dk)
+        {
+            return cn.getDatatable("SELECT MaLop FROM tblLop "+dk);
         }
     }
 }
